1239b89e0bace5a7eddd7cac9cec857564d7f20b
John Durham <kilo.kilo.delta8@gmail.com>
2021-02-16 14:10:36 -0500

    Started and added first problem set

--- main.cpp
+++ main.cpp
@@ -17,14 +17,19 @@ using namespace std;
 
 //Displays a main menu and returns users choice
 int displayMenu();
-
+//Multiples of 3 and 5 Project Euler problem #1
+void multiplesOfThreeAndFive();
 
 
 
 int main()
 {
     //var dec to hold the return value from the displayMenu() function
+    //and a counter to track the amount of wrong selections
     int choice;
+    int counter = 0;
+
+    //TODO - Size terminal window to suit the ascii menu's
 
 
     //keep running until the user wishes to exit
@@ -33,15 +38,29 @@ int main()
         //call the displayMenu() function
         choice = displayMenu();
 
-        //break out of the do-while if the user chose wrong 3 times
-        if (choice == -1){
-            break;
+        //validate the users choice
+        if (choice < 1 || choice > 12){
+            cout << "That is an invalid selection, try again!" << endl;
+            counter++;
         }
+
+        //break out of the do-while if the user chose wrong 3 times
+        if (counter == 3){
+            cout << "Perhaps it is time to hang it up for a bit." << endl;
+            cout << "Come back later and try again....." << endl;
+            return 0;
+        }
+
+
+        //clear(flush) the menu from the screen
+        system("clear");
+
 
 
         //filter choice var through switch statement
         switch (choice){
             case 1:
+                multiplesOfThreeAndFive();
                 break;
             case 2:
                 break;
@@ -66,11 +85,19 @@ int main()
         }
 
 
+        //clear(flush) the screen
+        system("clear");
+
+
     }while(choice != 12);
+
+
 
 
     //saying good-bye
     cout << "Good Bye!" << endl;
+
+
 
     return 0;
 }
@@ -88,33 +115,43 @@ int main()
 //If the user chooses an invalid selection three times, the program exits
 int displayMenu(){
 
-    //var dec to hold user choice and a counter for wrong selections
+    //var dec to hold user choice
     int choice;
-    int counter = 0;
 
-    //iterate until selection is satisfied or too many worng selections
-    do{
-        //dipslay menu
-        cout << "==================MENU==================" << endl;
-        cout << "|  1.                   7.             |" << endl;
-        cout << "|  2.                   8.             |" << endl;
-        cout << "|  3.                   9.             |" << endl;
-        cout << "|  4.                  10.             |" << endl;
-        cout << "|  5.                  11.             |" << endl;
-        cout << "|  6.                  12. EXIT        |" << endl;
-        cout << "========================================" << endl;
-        cout << "     Enter your selection wisely: ";
-        cin >> choice;
 
-        //validate the users choice
-        if (choice < 0 || choice > 12){
-            cout << "" << endl;
-            return -1;
-        }
-
-    }while(choice != 12);
+    //dipslay menu
+    cout << "=============================MENU=============================" << endl;
+    cout << "|  1. Multiples of 3 and 5        7.                         |" << endl;
+    cout << "|  2.                             8.                         |" << endl;
+    cout << "|  3.                             9.                         |" << endl;
+    cout << "|  4.                            10.                         |" << endl;
+    cout << "|  5.                            11.                         |" << endl;
+    cout << "|  6.                            12. EXIT                    |" << endl;
+    cout << "==============================================================" << endl;
+    cout << "                Enter your selection wisely: ";
+    cin >> choice;
 
     return choice;
+}
+
+
+//Multiples of 3 and 5 Project Euler problem #1 presentation
+void multiplesOfThreeAndFive(){
+    //explenation from site
+    cout << "                                MULTIPLES OF 3 AND FIVE                                        " << endl;
+    cout << "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9." << endl;
+    cout << "The sum of these multiples is 23.                                                              " << endl;
+    cout << "Find the sum of all the multiples of 3 or 5 below 1000.                                        " << endl;
+
+    //TODO - show code
+
+    //TODO - run code, displaying results
+
+
+    //linux command to pause the terminal screen
+    cout << endl;
+    cout << "Press any key to return to Menu!";
+    cin.ignore().get();
 }
 
 
